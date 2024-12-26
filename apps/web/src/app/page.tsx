86f3dfcd79a6bb5c7fdb49957ae8912e018e58dd
sub0ne1 <iamsubhajit.d@gmail.com>
2024-12-26 12:25:16 -0500

    tailwindcss and schadcn: done

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -5,10 +5,8 @@ export default function Home() {
   return (
     <main className="flex h-screen flex-col lg:px-32 py-4">
       <Header />
-      <Button variant="destructive">Sign Up</Button>
       <div className="flex flex-col items-center justify-center mb-10">
         <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex flex-col mb-10">
-          
           <h1 className="text-3xl font-black text-center text-transparent bg-clip-text bg-gradient-to-r from-gray-500 via-zinc-500 to-red-900">
             <span>find your dream job </span>
             <br />
--- apps/web/src/components/header.tsx
+++ apps/web/src/components/header.tsx
@@ -1,12 +1,18 @@
+import { Button } from "@/components/ui/button";
+
 export default function Header() {
   return (
     <nav className="">
       <div className="max-w-screen-xl flex flex-wrap justify-between mx-auto p-4">
         <a className="flex items-center space-x-1 rtl:space-x-reverse">
-          <span className="self-center text-3xl font-semibold font-black text-center sm:text-left text-transparent bg-clip-text bg-gradient-to-r from-gray-500 via-zinc-500 to-red-900">
+          <span className="bg-red self-center text-3xl font-semibold font-black text-center sm:text-left text-transparent bg-clip-text bg-gradient-to-r from-gray-500 via-zinc-500 to-red-900">
             indexone
           </span>
         </a>
+        <div className="flex justtify-center gap-2">
+          <Button size="sm">Sign In</Button>
+          <Button variant="outline" size="sm">Sign up</Button>
+        </div>
       </div>
     </nav>
   );
